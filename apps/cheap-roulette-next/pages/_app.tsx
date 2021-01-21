307792e4c148502df381e0c2639c04c511145c9a
Tekeshi Emoto <45647252+takeshiemoto@users.noreply.github.com>
2021-01-21 23:39:44 +0900

    Feature/added material UI (#3)
    * refactor: remove masic number
    
    * chore: add material-ui
    
    * feat: material-ui setup
    
    * feat: markup form

--- apps/cheap-roulette-next/pages/_app.tsx
+++ apps/cheap-roulette-next/pages/_app.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { SWRConfig } from 'swr';
+import { ThemeProvider } from 'styled-components';
+import { createMuiTheme } from '@material-ui/core';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'light',
+    },
+  });
+
   return (
     <>
       <Head>
         <title>Welcome to cheap-roulette-next!</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <SWRConfig
         value={{ fetcher: (args) => fetch(args).then((res) => res.json()) }}
       >
-        <Component {...pageProps} />
+        <ThemeProvider theme={darkTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </SWRConfig>
     </>
   );
