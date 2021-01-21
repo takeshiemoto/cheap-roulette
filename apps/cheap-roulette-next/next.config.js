9eafa8a80b51530de0984d8ae2b55709c2483bab
Tekeshi Emoto <45647252+takeshiemoto@users.noreply.github.com>
2021-01-21 14:43:24 +0900

    Feature/connect api (#2)
    * refactor: connect api
    
    * refactor: lint fix

--- apps/cheap-roulette-next/next.config.js
+++ apps/cheap-roulette-next/next.config.js
@@ -1,4 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
 
-module.exports = withNx({});
+dotenv.config();
+
+module.exports = withNx({
+  env: {
+    PROD_API_URL: process.env.PROD_API_URL,
+    DEV_API_URL: process.env.DEV_API_URL,
+  },
+});
