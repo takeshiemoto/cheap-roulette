cb35da7bb9563b6867cae5caf3b21d1a0a6ab0a4
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-01-22 13:45:49 +0900

    feat: added header

--- apps/cheap-roulette-next/pages/_app.tsx
+++ apps/cheap-roulette-next/pages/_app.tsx
@@ -3,7 +3,16 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { SWRConfig } from 'swr';
-import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import {
+  AppBar,
+  createMuiTheme,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { GitHub } from '@material-ui/icons';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   const theme = createMuiTheme({ palette: { type: 'dark' } });
@@ -20,6 +29,18 @@ function CustomApp({ Component, pageProps }: AppProps) {
         value={{ fetcher: (args) => fetch(args).then((res) => res.json()) }}
       >
         <ThemeProvider theme={theme}>
+          <AppBar color={'inherit'} position={'static'}>
+            <Toolbar>
+              <Typography variant={'h6'} style={{ flexGrow: 1 }}>
+                CHEAP ROULETTE
+              </Typography>
+              <IconButton
+                href={`https://github.com/takeshiemoto/cheap-roulette`}
+              >
+                <GitHub />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
